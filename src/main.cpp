c458180ef8225629f6f0883b7d7e245d76c96552
Vasco Costa <vascomacosta@gmail.com>
2026-07-22 22:46:39 +0100

    202607222246

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,34 @@ class Shape {
 public:
   virtual ~Shape() = default;
   virtual void draw() = 0;
-  virtual double area() = 0;
+  virtual double area() const = 0;
 };
 
 class Circle : public Shape {
 public:
   Circle(double radius) : radius_{radius} {}
   void draw() override { std::cout << "Circle\n"; }
-  double area() override { return Pi * std::pow(radius_, 2); }
+  double area() const override { return Pi * std::pow(radius_, 2); }
 
 private:
   double radius_;
 };
 
-int main() {}
+class Rectangle : public Shape {
+public:
+  Rectangle(double width, double height) : width_{width}, height_{height} {}
+  void draw() override { std::cout << "Rectangle\n"; }
+  double area() const override { return width_ * height_; }
+
+private:
+  double width_;
+  double height_;
+};
+
+int main() {
+  Circle c{10.5};
+
+  Shape &s = c;
+
+  s.draw();
+}
